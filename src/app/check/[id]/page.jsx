de8083aa7778da3c72652085797e6a7ad0a23b7e
Golam Muntakim <seams123456789@gmail.com>
2024-07-09 01:07:47 -0700

    confirm order send to the database

--- src/app/check/[id]/page.jsx
+++ src/app/check/[id]/page.jsx
@@ -1,12 +1,40 @@
+"use client"
 import { getServicesDetails } from '@/services/getServices';
-import React from 'react';
+import { useSession } from 'next-auth/react';
+import  { useEffect, useState } from 'react';
 
-const page = async({params}) => {
-    const details = await getServicesDetails(params.id)
-    const{_id, title, description, img, price, facility} = details.service;
+const page = ({params}) => {
+    const {data} = useSession()
+    const [service, setService] = useState({});
+    const loadService = async() =>{
+         const details = await getServicesDetails(params.id)
+         setService(details.service);
+    }
+    
+     const{_id, title, description, img, price, facility} = service || {};
     const handleBooking= async(event)=>{
         event.preventDefault()
+        const newBooking={
+            email: data?.user?.email,
+            name: data?.user?.name,
+            message: event.target.message.value,
+            date:event.target.date.value,
+            serviceTitle : title,
+            serviceId : _id,
+            price: price
+        }
+        const response = await fetch('http://localhost:3000/check/api/new-booking',{
+            method: 'POST',
+            body: JSON.stringify(newBooking),
+            headers:{
+                "content-type" : "application/json"
+            }
+        })
+        console.log(response)
     }
+    useEffect(()=>{
+        loadService()
+    },[params])
     return (
         <div className='bg-white'>
              <div>
@@ -18,10 +46,10 @@ const page = async({params}) => {
              <div className='mt-10 bg-[#F3F3F3] p-10 w-[800px] mx-auto'>
                 <form onSubmit={handleBooking}>
                   <div className='grid grid-cols-2 gap-4'>
-                  <input type="text" name="name" placeholder='First Name' className='w-[350px] p-4 rounded-md' />
-                  <input type="text" name="name" placeholder='Second Name' className='w-[350px] p-4 rounded-md' />
-                  <input type="number" name="phone" placeholder='Phone Number' className='w-[350px] p-4 rounded-md' />
-                  <input type="email" name="email" placeholder='Email' className='w-[350px] p-4 rounded-md' />
+                  <input defaultValue={data?.user?.name} type="text" name="name" placeholder=' Name' className='w-[350px] p-4 rounded-md' readOnly/>
+                  <input defaultValue={new Date().getDate()} type="date" name="date"  className='w-[350px] p-4 rounded-md' />
+                  <input defaultValue={price} type="number" name="price"  className='w-[350px] p-4 rounded-md' readOnly/>
+                  <input defaultValue={data?.user?.email} type="email" name="email" placeholder='Email' className='w-[350px] p-4 rounded-md' readOnly/>
                   </div>
                   <div className='mt-4 '>
                     <textarea className='w-[700px] h-[200px] rounded-md' placeholder='your message' type="text" name="message"></textarea>
