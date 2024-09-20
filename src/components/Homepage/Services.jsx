3374f0abda7a01399e33587168869271cbc27dee
Golam Muntakim <seams123456789@gmail.com>
2024-09-19 19:15:49 -0700

    edited

--- src/components/Homepage/Services.jsx
+++ src/components/Homepage/Services.jsx
@@ -16,7 +16,7 @@ const Services = async() => {
             <div className='text-center'>
                 <h1 className='text-[#FF3811]'>Service</h1>
                 <h1 className='font-bold text-5xl '>Our Service Area</h1>
-                <p className='text-[#737373]'>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+                <p className='text-[#737373]'>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
             </div>
             <div className='container mx-auto mt-12 grid grid-cols-1 lg:grid-cols-3 gap-6'>
                 {
